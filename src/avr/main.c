e9cbc9656434b5b5dfc11332ac5f789cf8b9fc4f
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2020-01-29 23:42:46 +0100

    TODO

--- src/avr/main.c
+++ src/avr/main.c
@@ -218,6 +218,7 @@ ISR(USART1_RX_vect) {
     AKAT_UNUSED u8 b = UDR1; // we must read here, no matter what, to clear interrupt flag
 
     // TODO: Implement RX-buffer...
+    // TODO: Find better description of results of 0x86 command...
 }
 
 // --- - - - - - - - - - - - TX
