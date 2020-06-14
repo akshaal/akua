5443fbf6896f9b729c207aab3f4174d9022938e6
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2020-06-14 13:07:02 +0200

    stuff

--- src/co2nn/trainco2.py
+++ src/co2nn/trainco2.py
@@ -1,78 +1,74 @@
 import tensorflow as tf
 import json
 
-# TODO: Cleanup
-
 # tf.debugging.set_log_device_placement(True)
 
-# TODO: Same as in JS
 PH_PREDICTION_WINDOW_LENGTH = 60
-
-# TODO: Move
-learning_rate = 5e-5
-
-unroll_gru = True
-
-model_fname = "data/co2-model.h5"
+PH_PREDICTION_FEATURES = 4
+MODEL_FNAME = "data/co2-model.h5"
 
 # ###############################################################################
+# ###############################################################################
+# ###############################################################################
+# ###############################################################################
+# MODEL CONSTRUCTION
 
 
-def create_model():
+def create_model(unroll_rrn: bool):
     input_layer = tf.keras.Input(
         name="Input",
-        shape=[PH_PREDICTION_WINDOW_LENGTH, 4]
+        shape=[PH_PREDICTION_WINDOW_LENGTH, PH_PREDICTION_FEATURES]
     )
 
-    gru1_layer = tf.keras.layers.Bidirectional(
-        name="Gru1",
-        layer=tf.keras.layers.GRU(
-            units=4,
+    rrn1_layer = tf.keras.layers.Bidirectional(
+        name="Rrn1",
+        layer=tf.keras.layers.LSTM(
+            units=2,
             activation="selu",
             kernel_initializer='lecun_normal',
             return_sequences=True,
-            unroll=unroll_gru
+            unroll=unroll_rrn
         )
     )(input_layer)
 
-    gru2_layer = tf.keras.layers.Bidirectional(
-        name="Gru2",
-        layer=tf.keras.layers.GRU(
-            units=4,
+    rrn2_layer = tf.keras.layers.Bidirectional(
+        name="Rrn2",
+        layer=tf.keras.layers.LSTM(
+            units=2,
             activation=None,
             kernel_initializer='lecun_normal',
             return_sequences=True,
-            unroll=unroll_gru
+            unroll=unroll_rrn
         )
-    )(gru1_layer)
+    )(rrn1_layer)
 
-    gru1n2_layer = tf.keras.layers.Add(
-        name="Gru1n2"
-    )([gru1_layer, gru2_layer])
+    rrn1n2_layer = tf.keras.layers.Add(
+        name="Rrn1n2"
+    )([rrn1_layer, rrn2_layer])
 
-    gru1n2_act_layer = tf.keras.layers.Activation(
-        name="Gru1n2Act",
+    rrn1n2_act_layer = tf.keras.layers.Activation(
+        name="Rrn1n2Act",
         activation="selu"
-    )(gru1n2_layer)
+    )(rrn1n2_layer)
 
-    # ---- GRU3 ----
+    # ---- rrn3 ----
 
-    gru3_layer = tf.keras.layers.Bidirectional(
-        name="Gru3",
-        layer=tf.keras.layers.GRU(
-            units=4,
+    rrn3_layer = tf.keras.layers.Bidirectional(
+        name="Rrn3",
+        layer=tf.keras.layers.LSTM(
+            units=2,
             activation="selu",
             kernel_initializer='lecun_normal',
-            unroll=unroll_gru
+            unroll=unroll_rrn
         )
-    )(gru1n2_act_layer)
+    )(rrn1n2_act_layer)
 
     fc1_layer = tf.keras.layers.Dense(
         name="FullyConnected1",
         units=3,
         activation="selu",
         kernel_initializer='lecun_normal'
-    )(gru3_layer)
+    )(rrn3_layer)
 
     output_layer = tf.keras.layers.Dense(
         name="Output",
@@ -90,6 +86,10 @@ def create_model():
     return model
 
 # ###############################################################################
+# ###############################################################################
+# ###############################################################################
+# ###############################################################################
+# DATASET
 
 
 def dataset_from_json(json):
@@ -101,31 +101,63 @@ def dataset_from_json(json):
         labels.append(item['ys'])
 
     dataset = tf.data.Dataset.from_tensor_slices((features, labels))
+    dataset = dataset.shuffle(10000)
     dataset = dataset.batch(len(features))
     dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
     return dataset
 
 
-with open('temp/co2-train-data.json') as f:
-    train_data = dataset_from_json(json.load(f))
+# ###############################################################################
+# ###############################################################################
+# ###############################################################################
+# ###############################################################################
+# TRAINING
 
-with open('temp/co2-valid-data.json') as f:
-    valid_data = dataset_from_json(json.load(f))
+def train(retrain: bool,
+          learning_rate: float,
+          unroll_rrn: bool,
+          epochs: int,
+          validation_freq: int):
+    with open('temp/co2-train-data.json') as f:
+        train_data = dataset_from_json(json.load(f))
 
-optimizer = tf.keras.optimizers.Adam(learning_rate)
+    with open('temp/co2-valid-data.json') as f:
+        valid_data = dataset_from_json(json.load(f))
 
-model = tf.keras.models.load_model(model_fname, compile=False)
-model.compile(loss="mean_squared_error", optimizer=optimizer)
-model.summary()
+    if retrain:
+        print("##### CREATING NEW NETWORK")
+        model = create_model(unroll_rrn=unroll_rrn)
+    else:
+        print("##### LOADING EXISTING NETWORK")
+        model = tf.keras.models.load_model(MODEL_FNAME, compile=False)
 
-print("Num GPUs Available: ", len(
-    tf.config.experimental.list_physical_devices('GPU')))
+    optimizer = tf.keras.optimizers.Adam(learning_rate)
 
-model.fit(
-    x=train_data,
-    validation_data=valid_data,
-    epochs=50_000,  # TODO: Move
-)
+    model.compile(loss="mean_squared_error", optimizer=optimizer)
+    model.summary()
 
-model.save(model_fname, save_format='h5')
+    model.fit(
+        x=train_data,
+        validation_data=valid_data,
+        epochs=epochs,
+        validation_freq=validation_freq,
+    )
+
+    model.save(MODEL_FNAME, save_format='h5')
+
+
+# ###############################################################################
+# ###############################################################################
+# ###############################################################################
+# ###############################################################################
+# MAIN
+
+if __name__ == '__main__':
+    train(
+        retrain=False,
+        learning_rate=1e-4,
+        unroll_rrn=True,
+        epochs=5000,
+        validation_freq=20
+    )
