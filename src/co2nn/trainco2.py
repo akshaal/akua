645d7e6010e79ef0e3fb213442da713ec02180f9
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2020-06-22 17:48:04 +0200

    early stop and such

--- src/co2nn/trainco2.py
+++ src/co2nn/trainco2.py
@@ -16,87 +16,109 @@ MODEL_FNAME = "data/co2-model.h5"
 # MODEL CONSTRUCTION
 
 
-def create_model(unroll_rrn: bool):
+def create_model():
+    def mk_conv1d_layers(input_layer,
+                         strides: int,
+                         kernel_size: int,
+                         filters: int,
+                         name: str,
+                         avgpool_strides: int = 0,
+                         avgpool_size: int = 0,
+                         maxpool_strides: int = 0,
+                         maxpool_size: int = 0,
+                         dropout: float = 0):
+        layer = tf.keras.layers.Conv1D(
+            name=name + "Conv",
+            kernel_size=kernel_size,
+            strides=strides,
+            filters=filters,
+            activation="selu",
+            kernel_initializer='lecun_normal'
+        )(input_layer)
+
+        if dropout > 0:
+            layer = tf.keras.layers.Dropout(
+                name=name + "Dropout",
+                rate=dropout
+            )(layer)
+
+        if avgpool_size > 0:
+            layer = tf.keras.layers.AvgPool1D(
+                name=name + "AvgPool",
+                pool_size=avgpool_size,
+                strides=avgpool_strides
+            )(layer)
+
+        if maxpool_size > 0:
+            layer = tf.keras.layers.MaxPool1D(
+                name=name + "MaxPool",
+                pool_size=maxpool_size,
+                strides=maxpool_strides
+            )(layer)
+
+        return layer
+
+    def mk_fc_layers(input_layer, units: int, name: str, dropout: float = 0):
+        layer = tf.keras.layers.Dense(
+            name=name + "FC",
+            units=units,
+            activation="selu",
+            kernel_initializer='lecun_normal'
+        )(input_layer)
+
+        if dropout > 0:
+            layer = tf.keras.layers.Dropout(
+                name=name + "Dropout",
+                rate=dropout
+            )(layer)
+
+        return layer
+
     input_layer = tf.keras.Input(
         name="Input",
         shape=[PH_PREDICTION_WINDOW_LENGTH, PH_PREDICTION_FEATURES]
     )
 
-    conv0_layer = tf.keras.layers.Conv1D(
-        name="Conv0",
+    conv0_layer = mk_conv1d_layers(
+        name="L0",
+        input_layer=input_layer,
         kernel_size=1,
         strides=1,
         filters=2,
-        activation="selu",
-        kernel_initializer='lecun_normal',
-    )(input_layer)
+    )
 
-    conv1_layer = tf.keras.layers.Conv1D(
-        name="Conv1",
+    conv1_layer = mk_conv1d_layers(
+        name="L1",
+        input_layer=conv0_layer,
         kernel_size=4,
-        strides=4,
-        filters=4,
-        activation="selu",
-        kernel_initializer='lecun_normal',
-    )(conv0_layer)
-
-    conv2_layer = tf.keras.layers.Conv1D(
-        name="Conv2",
-        kernel_size=1,
         strides=1,
         filters=4,
-        activation="selu",
-        kernel_initializer='lecun_normal',
-    )(conv1_layer)
+        dropout=0.75,
+        maxpool_size=4,
+        maxpool_strides=2
+    )
 
-    conv2n1_layer = tf.keras.layers.Add(
-        name="Add2n1"
-    )([conv2_layer, conv1_layer])
-
-    act2n1_layer = tf.keras.layers.Activation(
-        name='Act2n1',
-        activation="selu"
-    )(conv2n1_layer)
-
-    conv3_layer = tf.keras.layers.Conv1D(
-        name="Conv3",
+    conv2_layer = mk_conv1d_layers(
+        name="L2",
+        input_layer=conv1_layer,
         kernel_size=3,
-        strides=3,
-        filters=10,
-        activation="selu",
-        kernel_initializer='lecun_normal',
-    )(act2n1_layer)
-
-    # (AlphaDropout works strange for some strange reason)
-    drop3_layer = tf.keras.layers.Dropout(
-        name="Dropout3",
-        rate=0.8
-    )(conv3_layer)
+        strides=1,
+        filters=6,
+        dropout=0.75,
+        maxpool_size=3,
+        maxpool_strides=3,
+    )
 
     flatten_layer = tf.keras.layers.Flatten(
         name="Flatten"
-    )(drop3_layer)
-
-    fc1_layer = tf.keras.layers.Dense(
-        name="FC1",
-        units=4,
-        activation="selu",
-        kernel_initializer='lecun_normal'
-    )(flatten_layer)
-
-    preoutput_layer = tf.keras.layers.Dense(
-        name="PreOutput",
-        units=1,
-        activation="selu",
-        kernel_initializer='lecun_normal'
-    )(fc1_layer)
+    )(conv2_layer)
 
     output_layer = tf.keras.layers.Dense(
         name="Output",
         units=1,
         activation="selu",
         kernel_initializer='lecun_normal'
-    )(preoutput_layer)
+    )(flatten_layer)
 
     model = tf.keras.Model(
         name="co2-predict2",
@@ -137,10 +159,10 @@ def dataset_from_json(json):
 
 def train(retrain: bool,
           learning_rate: float,
-          unroll_rrn: bool,
           epochs: int,
           validation_freq: int,
-          tensorboard: bool):
+          tensorboard: bool,
+          early_stop_epoch_patience: float):
     with open('temp/co2-train-data.json') as f:
         train_data = dataset_from_json(json.load(f))
 
@@ -149,7 +171,7 @@ def train(retrain: bool,
 
     if retrain:
         print("##### CREATING NEW NETWORK")
-        model = create_model(unroll_rrn=unroll_rrn)
+        model = create_model()
     else:
         print("##### LOADING EXISTING NETWORK")
         model = tf.keras.models.load_model(MODEL_FNAME, compile=False)
@@ -176,6 +198,7 @@ def train(retrain: bool,
     model.summary()
 
     callbacks = []
+
     if tensorboard:
         print("ENABLING TENSORBOARD MAKES TRAINING SLOWER!!!!!!!!!")
         logdir = "temp/logs/co2-fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -184,6 +207,32 @@ def train(retrain: bool,
                 log_dir=logdir,
                 write_images=True,
                 write_graph=True,
+            )
+        )
+
+    if early_stop_epoch_patience > 0:
+        # Custom class to avoid having errors because validation_freq
+        class CustomEarlyStopping(tf.keras.callbacks.EarlyStopping):
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+                self.__error_given = False
+
+            def get_monitor_value(self, logs):
+                logs = logs or {}
+                monitor_value = logs.get(self.monitor)
+                if monitor_value is None:
+                    if not self.__error_given:
+                        self.__error_given = True
+                        return super().get_monitor_value(logs)
+                    return None
+                return monitor_value
+
+        callbacks.append(
+            CustomEarlyStopping(
+                monitor='val_loss',
+                patience=early_stop_epoch_patience,
+                restore_best_weights=True,
+                verbose=True
             )
         )
 
@@ -206,10 +255,10 @@ def train(retrain: bool,
 
 if __name__ == '__main__':
     train(
-        retrain=False,
-        learning_rate=1e-5,
-        unroll_rrn=True,
+        retrain=True,
+        learning_rate=1e-2,
         epochs=300_000,
         validation_freq=100,
-        tensorboard=False
+        tensorboard=False,
+        early_stop_epoch_patience=30_000
     )
