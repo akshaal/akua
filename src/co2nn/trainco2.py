15b66fde69b5d03f3a86abd1dc9618c1dc8ce733
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2020-06-24 11:59:07 +0200

    val_loss 1.1965e-4

--- src/co2nn/trainco2.py
+++ src/co2nn/trainco2.py
@@ -307,12 +307,12 @@ def train(retrain: bool,
 if __name__ == '__main__':
     train(
         retrain=False,
-        learning_rate=1e-4,
-        weight_decay_lr_multiplier=0.1 * np.random.random(),
+        learning_rate=1e-2,
+        weight_decay_lr_multiplier=0.01 * np.random.random(),
         epochs=600_000,
         first_decay_epochs=30_000,
         validation_freq=20,
         tensorboard=False,
         early_stop_epoch_patience=100_000,
-        opt="sgd"
+        opt="adam"
     )
